96884d8031d82a32fea8005d7c48c33ec84ee831
Louis Muhr <louismuhr8@gmail.com>
2025-12-07 22:57:43 +0100

    structure changes

--- app/admin/route.ts
+++
@@ -1,8 +0,0 @@
-// app/admin/route.ts
-import { NextResponse } from "next/server";
-
-export async function GET() {
-  return NextResponse.redirect(
-    new URL("/api/admin", "http://localhost:3000")
-  );
-}
