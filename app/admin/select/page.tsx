6613d29ecd715cc85793b2e02b4f201dc37977d2
Louis Muhr <louismuhr8@gmail.com>
2025-12-11 19:14:01 +0100

    image wird bei produkt Entfernung mit entfernt

--- app/admin/select/page.tsx
+++ app/admin/select/page.tsx
@@ -3,6 +3,8 @@
 import AdminGuard from "@/components/AdminGuard";
 import AdminSelect from "@/components/AdminSelect";
 
+export const dynamic = "force-dynamic";
+
 export default function AdminSelectPage() {
   return (
     <AdminGuard>
